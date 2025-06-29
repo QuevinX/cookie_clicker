d5c92713d9cad8936d811c3eb01a8e613f29dbcc
Quevin <qacustodio@up.edu.ph>
2025-06-29 13:12:19 +0800

    feat: added a terminate key to end auto clicker on cookie

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -1,3 +1,4 @@
+import threading
 import pyautogui
 import time
 from pynput import keyboard
@@ -5,6 +6,7 @@ import sys
 
 target_color = (90, 52, 43)
 clicking = False
+stop_clicking = False
 
 def mouse_pos():
     print("Move your mouse around to find the x and y positions")
@@ -17,34 +19,42 @@ def mouse_pos():
         print("\nTracking Stopped")
         return
 
-def auto_clicker(key):
-    global clicking
-    
+def auto_clicker():
+    global stop_clicking, clicking
+    x, y = 160, 370
+    width, height = 100, 110
+
+    cropped = pyautogui.screenshot(region=(x, y, width, height))
+    cropped.save("/Users/quevin.custodio/Desktop/autoclick.png")
+
+    for i in range(100):
+        if stop_clicking:
+            print("\nClicking stopped")
+            break
+
+        pyautogui.click(x=180, y=450)
+        print(f"Clicked {i + 1} times", end="\r")
+
+    clicking = False
+    stop_clicking = False
+    print("\nAuto Click Finished")
+
+def on_key_press(key):
+    global clicking, stop_clicking
+
     try:
-        if key.char == "1" and not clicking:
+        if key.char == "1":
             clicking = True
-            print("\nAuto Click Started")
-            x, y = 160, 370
-            width, height = 100, 110
-
-            cropped = pyautogui.screenshot(region=(x, y, width, height))
-            cropped.save("/Users/quevin.custodio/Desktop/autoclick.png")
-
-            for i in range(100):
-                pyautogui.click(x=180, y=450)
-                print(f"Clicked {i + 1} times", end="\r")
-                time.sleep(0.1)
-
-                if key.char == "q":
-                    print("\nExiting program...")
-                    return False  # Stops the listener cleanly
-
-            clicking = False
-            print("\nAuto Click Finished")
-
+            threading.Thread(target=auto_clicker).start()
+        
+        elif key.char == "q":
+            print("\nEnded program")
+            stop_clicking = True
+            return False
 
     except AttributeError:
-        pass  # Handles non-char keys like shift, etc.
+        pass
+    
 
 def scanner(target_color, play_sound):
     time.sleep(3)
@@ -65,14 +75,13 @@ def scanner(target_color, play_sound):
                 cropped_image.save("/Users/quevin.custodio/Desktop/cropped.png")
                 return
     print("Color not found on screen")
-
-# Start listener
-print("Press '1' to auto-click. Press 'q' to quit.")
-with keyboard.Listener(on_press=auto_clicker) as listener:
+    1
+print("Press '1' to start auto-clicking. Press 'q' to stop and exit.")
+with keyboard.Listener(on_press=on_key_press) as listener:
     listener.join()
 
 # mouse_pos()  # Uncomment to use
 
 # time.sleep(3)
 # pyautogui.moveTo(160, 370)
-# print("Move cursor")
+# print("Move cursor")z
